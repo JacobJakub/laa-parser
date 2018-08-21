7ee421b9ce1623c9d3cb5f46099fe7f1140b70f4
Jakub Racek <me@jakubracek.net>
2018-08-21 11:42:43 +0200

    Add database

--- request.py
+++ request.py
@@ -1,10 +1,11 @@
 import requests
 
 from parse import get_member_test_ids, get_questions_context
+from session import requests_retry_session
 
 
 def generate_member_test_uids(test_id):
-    html_res = requests.post(
+    html_res = requests_retry_session().post(
         "https://zkouseni.laacr.cz/Zkouseni/index.html?page=volnytest",
         data=dict(test=test_id, ok="OK"),
         allow_redirects=True
@@ -13,7 +14,7 @@ def generate_member_test_uids(test_id):
     return (test_id,) + get_member_test_ids(html_res)
 
 def get_results(test_id, member_id, test_uid):
-    html_res = requests.post(
+    html_res = requests_retry_session().post(
         "https://zkouseni.laacr.cz/Zkouseni/index.html?page=provedtest",
         data=dict(
             test=test_id,
--- statistics.py
+++ statistics.py
@@ -1,3 +1,5 @@
+import pickle
+
 from request import generate_member_test_uids, get_results
 from collections import OrderedDict
 from sys import argv
@@ -9,19 +11,21 @@ def questions_with_answers(test_id):
     a,b,c = generate_member_test_uids(test_id)
     return get_results(a,b,c)
 
-def add_questions(test_id, num):
-    data = []
+def add_questions(data, test_id, num):
+    data['iterations'] += num
+
     for i in range(num):
         print('Doing test ', i)
         for q in questions_with_answers(test_id):
             try:
-                existing = next(item for item in data if item["question"] == q["question"])
+                existing = next(item for item in data['qas'] if item["question"] == q["question"])
                 existing["count"] += 1
             except StopIteration:
                 q.update({"count": 1})
-                data.append(q)
+                data['qas'].append(q)
     return data
 
+# Writes final markdown from question_asnwers and adds few extra statistics
 def markdown_questionanswers(question_answers, iterations):
     lines = []
 
@@ -32,21 +36,57 @@ def markdown_questionanswers(question_answers, iterations):
         answers = qa['answers']
 
         lines.append("#### %s \n" % question)
-        lines.append("*(%s/%s)* \n" % (qa['count'], iterations))
+        lines.append(
+            "*(%s/%s) - %s%%* \n" % (
+                qa['count'],
+                iterations,
+                round((qa['count'] / iterations * 100), 1)
+            )
+        )
         for a in answers:
             if a["is_correct"]:
-                lines.append("* **%s** \n" % a['text'])
+                lines.append("* **%s** \n" % a['text'].rstrip())
             else:
-                lines.append("* %s \n" % a['text'])
+                lines.append("* %s \n" % a['text'].rstrip())
         lines.append("\n")
 
     return lines
 
+def _get_db_path(test_id):
+    return r"database/%s.data" % test
+
+def read_database(test_id):
+    import os
+
+    path = _get_db_path(test_id)
+
+    if not os.path.exists(path):
+        os.mknod(path)
+        return {'iterations': 0, 'qas': []}
+    else:
+        with open(path, "rb") as f:
+          return pickle.load(f)
+
+def write_database(data, test_id):
+    with open(_get_db_path(test_id), "wb") as f:
+        pickle.dump(data, f)
+
 if __name__ == "__main__":
-    data = add_questions(test, iterations)
+    # Read from database
+    data = read_database(test)
 
-    data = sorted(data, key=lambda t: t['count'], reverse=True)
-    markdown = markdown_questionanswers(data, iterations)
+    # Append new data
+    data = add_questions(data, test, iterations)
 
+    #Store to database
+    write_database(data, test)
+
+    # Sort questions by occurence
+    qas = sorted(data['qas'], key=lambda t: t['count'], reverse=True)
+
+    # Translate to markdown
+    markdown = markdown_questionanswers(qas, data['iterations'])
+
+    #Write results
     with open(r"results/%s.md" % test, "w") as f:
         f.writelines(markdown)
